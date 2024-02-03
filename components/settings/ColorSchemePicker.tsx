e32de0f6206571c1f66031473bb16648099b21e2
kelvinleandro <kelvinleandro5@gmail.com>
2024-02-03 09:42:25 -0300

    refactor: settings components with .tsx

--- components/settings/ColorSchemePicker.tsx
+++ components/settings/ColorSchemePicker.tsx
@@ -1,8 +1,8 @@
-import { useContext } from 'react';
+import React, { useContext } from 'react';
 import RNPickerSelect from 'react-native-picker-select';
-import { ColorSchemeContext } from "../../context/ColorSchemeContext"
+import { ColorSchemeContext } from "../../context/ColorSchemeContext";
 
-const ColorSchemePicker = () => {
+const ColorSchemePicker: React.FC = () => {
   const colorSchemeCtx = useContext(ColorSchemeContext);
 
   return (
